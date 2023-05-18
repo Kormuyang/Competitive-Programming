6a3f5d225152f954051ebbb37d7b9b17e6276dd9
Kormuyang <95235409+Kormuyang@users.noreply.github.com>
2023-05-18 12:47:10 +0700

    Update codecube_057.cpp

--- beta/codecube_057.cpp
+++ beta/codecube_057.cpp
@@ -12,6 +12,8 @@ const int MOD = 1e6 + 7;
 const int mxN = 2e5 + 1;
 const int mxB = 1e2 + 1;
 
+const int di[8][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
+
 int a[mxN], dp[2][mxB], mn[2][mxB];
 int n, k, ans = INF;
 
