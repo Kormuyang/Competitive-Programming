cda7a640c994c6d0df095bd42b04c1d4d92620dd
Kormuyang <95235409+Kormuyang@users.noreply.github.com>
2023-05-20 17:06:33 +0700

    Create mineral.cpp

---
+++ crackncode-th/Pre-TOI19/mineral.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define pi pair<int, int>
+#define t3 tuple<int, int, int>
+#define sz(a) ((int) a.size())
+
+const int mxN = 1e5 + 1;
+
+struct Fenwick {
+    int t[mxN];
+    void init() { for(int i = 0; i < mxN; i++) t[i] = 0; return; }
+    void add(int idx, int val) { for(; idx < mxN; idx += (idx & -idx)) t[idx] += val; return; }
+    int read(int idx) { int ret = 0; for(; idx > 0; idx -= (idx & -idx)) ret += t[idx]; return ret; }
+} f;
+
+int a[mxN], b[mxN];
+int n, m, ans;
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+    cin >> n >> m;
+    for(int i = 1; i <= n; i++) cin >> a[i], a[i] *= 1000;
+    for(int i = 1; i <= n; i++) cin >> b[i];
+    int l = 0, r = 1e7;
+    while(l <= r) {
+        int md = (l + r) / 2;
+        vector<ll> qs(n + 1), v(n + 1);
+        for(int i = 1; i <= n; i++) v[i] = qs[i] = qs[i - 1] + a[i] - 1ll * b[i] * md;
+        sort(v.begin(), v.end());
+        f.init();
+        ll res = 0;
+        for(int i = 0; i <= n; i++)  {
+            int idx = lower_bound(v.begin(), v.end(), qs[i]) - v.begin() + 1;
+            res += f.read(idx); f.add(idx, 1);
+        }
+        if(res >= m) ans = md, l = md + 1;
+        else r = md - 1;
+    }
+    cout << fixed << setprecision(3) << ans / 1000.0 << '\n';
+    return 0;
+}
