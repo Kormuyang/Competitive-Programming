a4764ff3efb204792536979f196a4c1b3d63f873
Kormuyang <95235409+Kormuyang@users.noreply.github.com>
2023-06-08 01:16:51 +0700

    Update toi19_explorer.cpp

--- toi/toi19_explorer.cpp
+++ toi/toi19_explorer.cpp
@@ -23,8 +23,8 @@ void dfs(int u, int p) {
 int32_t main() {
     cin.tie(nullptr)->sync_with_stdio(false);
     fac[0] = 1; for(int i = 1; i < mxN; i++) fac[i] = (fac[i - 1] * i) % MOD;
-    int n; cin >> n; int prv; cin >> prv;
-    for(int i = 0, x; i < 2 * (n - 1); i++) { cin >> x; adj[prv].emplace_back(x); prv = x; }
+    int n; cin >> n; int u; cin >> u;
+    for(int i = 0, v; i < 2 * (n - 1); i++) { cin >> v; adj[u].emplace_back(v); u = v; }
     dfs(1, 0);
     cout << ans << '\n';
     return 0;
